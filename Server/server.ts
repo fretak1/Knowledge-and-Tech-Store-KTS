7c601c00ab913f72c9a128112cc7be50ac6a56bc
frezer takele <129789211+fretak1@users.noreply.github.com>
2026-02-02 22:53:24 +0300

    Add CORS middleware with options to server

--- Server/server.ts
+++ Server/server.ts
@@ -31,6 +31,8 @@ const corsOptions = {
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
 };
+
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(cookieParser());
 
